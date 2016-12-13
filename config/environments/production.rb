a087e2a250f9df95a56e2ec7c09328d28e8e9f18
abeeku <KwekuBondzie@gmail.com>
2016-12-13 14:35:44 -0500

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,4 @@
-Agendar::Application.configure do
+ExecutiveFunction::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
   # Code is not reloaded between requests
